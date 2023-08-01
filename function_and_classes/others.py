08277ae17cd30f4d5f1c8bff4823e06bf794455e
Enri-gin <genricog00@gmail.com>
2023-08-01 19:28:15 +0200

    Add get param and macs

--- function_and_classes/others.py
+++ function_and_classes/others.py
@@ -1,5 +1,7 @@
 from .Blocks import *
 import numpy as np
+import torch
+from thop import profile
 
 
 def disable_batch_norm(blocks_model):
@@ -18,3 +20,10 @@ def count_params(model: nn.Module):
     model_parameters = filter(lambda p: p.requires_grad, model.parameters())
     params = sum([np.prod(p.size()) for p in model_parameters])
     return params.item()
+
+
+def get_macs_and_params(model: nn.Module, input_shape: list):
+    model_device = next(model.parameters()).device
+    input_ = torch.rand(input_shape, device=model_device)
+    macs, params = profile(model, inputs=(input_,), verbose=False)
+    return macs, params
--- prova_log_synflow.py
+++ prova_log_synflow.py
@@ -1,15 +1,15 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
-import numpy as np
+# import torch
+# import torch.nn as nn
+# import torch.optim as optim
+# from torch.optim import lr_scheduler
+# import numpy as np
 import torchvision
-from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
-import pyvww
-import time
-import os
-import copy
+# from torchvision import datasets, models, transforms
+# import matplotlib.pyplot as plt
+# import pyvww
+# import time
+# import os
+# import copy
 from zero_cost_nas.foresight.pruners.predictive import find_measures
 from function_and_classes.model_by_blocks import ModelByBlocks
 from function_and_classes.others import *
@@ -37,6 +37,10 @@ disable_batch_norm(model)
 
 measures = find_measures(model, dataloader, dataload_info=('random', n_batches, num_classes),
                          device=device, measure_names=measure_names)
+enable_batch_norm(model)
 
 score = measures['synflow']
 print(f'LogSynflow score = {score}')
+
+masc, n_param = get_macs_and_params(model, [16, 3, 96, 96])
+print(f'model has {masc:.2f} MACs and {n_param} parameters')
