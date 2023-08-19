3920f36424f69de4f5b9b65b5ad04dece1d1dca2
Enri-gin <genricog00@gmail.com>
2023-08-19 15:13:35 +0200

    Add NASWOT metric

--- NASWOT.py
+++ NASWOT.py
@@ -1,35 +1,34 @@
-import numpy as np
-from torch import nn
+import torch
+import torch.nn as nn
 
 
-def compute_NASWOT(model: nn.Module, dataloader_train):
+def compute_naswot_score(net: nn.Module, inputs: torch.Tensor, targets: torch.Tensor, device: torch.device):
+    with torch.no_grad():
+        codes = []
 
-    x, target = next(iter(dataloader_train))
-    batch_size = x.shape[0]
+        def hook(self: nn.Module, m_input: torch.Tensor, m_output: torch.Tensor):
+            code = (m_output > 0).flatten(start_dim=1)
+            codes.append(code)
 
-    def counting_forward_hook(module, inp, out):
-        inp = inp[0].reshape(inp[0].size(0), -1)
-        x = (inp > 0).float()  # binary indicator
-        K = x @ x.t()
-        K2 = (1. - x) @ (1. - x.t())
-        model.K = model.K + K.cpu().numpy() + K2.cpu().numpy()
+        hooks = []
+        for m in net.modules():
+            if isinstance(m, nn.ReLU):
+                hooks.append(m.register_forward_hook(hook))
 
-    def counting_backward_hook(module, inp, out):
-        module.visited_backwards = True
+        _ = net(inputs)
 
-    # This is the logarithm of the determinant of K
-    def hooklogdet(K, labels=None):
-        s, ld = np.linalg.slogdet(K)
-        return ld
+        for h in hooks:
+            h.remove()
 
-    model.K = np.zeros((batch_size, batch_size))
-    for name, module in model.named_modules():
-        if 'ReLU' in str(type(module)):
-            module.register_backward_hook(counting_backward_hook)
-            module.register_forward_hook(counting_forward_hook)
+        full_code = torch.cat(codes, dim=1)
 
-    # run batch through network
-    model(x)
+        # Fast Hamming distance matrix computation
+        del codes, _
+        full_code_float = full_code.float()
+        k = full_code_float @ full_code_float.t()
+        del full_code_float
+        not_full_code_float = torch.logical_not(full_code).float()
+        k += not_full_code_float @ not_full_code_float.t()
+        del not_full_code_float
 
-    score = hooklogdet(model.K, target)
-    return score
+        return torch.slogdet(k).logabsdet.item()
